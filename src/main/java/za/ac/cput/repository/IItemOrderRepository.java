d8ad83b4300d80d3989e150e4563874ca8260eba
OnlyBlaine <blainesimpson.bs@gmail.com>
2022-08-06 16:23:00 +0200

    Menu and ItemOrder Repo done

--- src/main/java/za/ac/cput/repository/IItemOrderRepository.java
+++ src/main/java/za/ac/cput/repository/IItemOrderRepository.java
@@ -1,16 +1,16 @@
 package za.ac.cput.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.cput.entity.ItemOrder;
 
 import java.util.Set;
 /* IItemOrderRepository.java
  Entity for the ItemOrder
  Author: Blaine Simpson (218020171)
- Date: 01 April 2022
+
 
  */
 
-public interface IItemOrderRepository extends IRepository<ItemOrder,String>{
-    public Set<ItemOrder> getAll();
+public interface IItemOrderRepository extends JpaRepository<ItemOrder,String> {
 //create,update,read,delete
 }
--- src/main/java/za/ac/cput/repository/IMenuRepository.java
+++ src/main/java/za/ac/cput/repository/IMenuRepository.java
@@ -1,17 +1,16 @@
 package za.ac.cput.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.cput.entity.Menu;
 
 import java.util.Set;
 /* IMenuRepository.java
  Entity for the Menu
  Author: Blaine Simpson (218020171)
- Date: 02 April 2022
+
 
  */
-public interface IMenuRepository extends IRepository<Menu, String> {
-
-    public Set<Menu> getAll();
+public interface IMenuRepository extends JpaRepository<Menu, String> {
 
     //create,update,read,delete
 
