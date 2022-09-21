73f9a1a0908a08c0ab89367ed3473dcd420ab6a2
DemiFarquhar <220322104@mycput.ac.za>
2022-09-21 15:04:04 +0200

    update factory of table and restaurant

--- src/main/java/za/ac/cput/util/LoginHelper.java
+++ src/main/java/za/ac/cput/util/LoginHelper.java
@@ -5,7 +5,30 @@
 */
 package za.ac.cput.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class LoginHelper {
+    public static boolean isEmptyOrNull(String str){
+        return StringUtils.isEmpty(str);
+    }
+
+    public static String setEmptyIfNull(String str){ //Method to make null values have empty Strings instead.
+        if (isEmptyOrNull(str)){
+            return StringUtils.EMPTY;
+        }
+        return str;
+    }
+
+    public static void checkStringParam(String paramName, String paramValue){ //Ensure that value cannot have null or empty value.
+        if (isEmptyOrNull(paramValue))
+            throw new IllegalArgumentException(String.format("Invalid value for: %s", paramName));
+    }
+    public static void checkIfObjectNull(String objectName, Object object){ //Checks object attributes to ensure they are not null.
+        if (object == null){
+            throw new IllegalArgumentException(String.format("%s is null", objectName));
+        }
+    }
+
 
     public static boolean isNull(String s){
         return s == null || s.isEmpty() || s.equals("") || s.equals(" ");
