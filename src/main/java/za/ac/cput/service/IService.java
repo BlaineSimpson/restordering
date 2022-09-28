273389f27badbc61caa05e8adb904e13a72b19f5
Craig Jarvis <craigjarvis25@gmail.com>
2022-09-28 17:06:47 +0200

    Created test classes

--- src/main/java/za/ac/cput/service/IService.java
+++ src/main/java/za/ac/cput/service/IService.java
@@ -1,6 +1,6 @@
 package za.ac.cput.service;
 
-import za.ac.cput.entity.Item;
+import za.ac.cput.entity.AdminLogin;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,9 +13,9 @@ import java.util.Optional;
 
 public interface IService<T,ID>{
     T save(T t);
-    Optional<Item> findById(ID id);
+    Optional<T> findById(ID id);
     // T update(T t);
     void delete(T t);
-    List<Item> findAll();
+    List<T> findAll();
     void deleteById(ID id);
 }
