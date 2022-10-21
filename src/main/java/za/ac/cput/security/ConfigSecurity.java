2440d19f76c9f0a2ec9448c94090b6d133aab4d1
DemiFarquhar <220322104@mycput.ac.za>
2022-10-21 18:20:48 +0200

    Security for customer

--- src/main/java/za/ac/cput/security/ConfigSecurity.java
+++ src/main/java/za/ac/cput/security/ConfigSecurity.java
@@ -90,10 +90,10 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET,"/restaurant/order/all").hasRole("USER")
                 .antMatchers(HttpMethod.DELETE,"restaurant/order/delete/").hasRole("USER")
                 //Customer
-                .antMatchers(HttpMethod.POST, "/restaurant/inventory/save").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/restaurant/inventory/find/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/restaurant/inventory/all").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/restaurant/inventory/delete/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/restaurant/customer/save").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/restaurant/customer/find/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/restaurant/customer/all").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/restaurant/customer/delete/").hasRole("ADMIN")
                // Bill
                 .antMatchers(HttpMethod.POST, "/restaurant/bill/save").hasRole("USER")
                 .antMatchers(HttpMethod.GET,"/restaurant/bill/find/").hasRole("USER")
